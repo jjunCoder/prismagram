eac894326a415e9fbcdd58e093a33d318fe31faa
jjuncoder <jjune21@gmail.com>
2020-02-29 19:14:04 +0900

    update prisma datamodel

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 require("dotenv").config();
-import {GraphQLServer} from 'graphql-yoga';
+import {
+    GraphQLServer
+} from 'graphql-yoga';
 import schema from "./schema";
 import logger from "morgan";
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({schema});
+const server = new GraphQLServer({
+    schema
+});
 
 server.express.use(logger("dev"));
-
+server.start()
 server.start({
     port: PORT
 }).then(r => () => console.log(`✅ Server running on http://localhost:${PORT}`));

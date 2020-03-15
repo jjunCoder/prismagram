908efa2f2d684097d8617ed7a5fe0c7773d4ac0c
jjuncoder <jjune21@gmail.com>
2020-03-15 15:11:20 +0900

    update API for seeRoom(s)

--- src/fragments.js
+++ src/fragments.js
@@ -37,11 +37,25 @@ export const FULL_POST_FRAGMENT = `
     }    
 `;
 
+export const MESSAGE_FRAGMENT = `
+    id
+    text
+    from {
+        ${USER_FRAGMENT}
+    }
+    to {
+        ${USER_FRAGMENT}
+    }
+`;
+
 export const ROOM_FRAGMENT = `
     fragment RoomParts on Room {
         id
         participants {
             ${USER_FRAGMENT}
+        }
+        messages {
+            ${MESSAGE_FRAGMENT}
         }
     }
 `;
